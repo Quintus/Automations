788e92f6be94dae826b25a9fde4eca79de8c333d
Marvin Gülker <sutniuq@gmx.net>
2009-09-03 16:56:43 +0000

    initial import

---
+++ lib/xdo.rb
@@ -0,0 +1,28 @@
+#Encoding: UTF-8
+#This file is part of Xdo. 
+#Copyright © 2009 Marvin Gülker
+#  Initia in potestate nostra sunt, de eventu fortuna iudicat. 
+
+#The namespace of this library. 
+module XDo
+  
+  #The command to start xdotool. 
+  XDOTOOL = "xdotool"
+  
+  #The command to start xsel. 
+  XSEL = "xsel"
+  
+  #The command to start xwininfo. 
+  XWININFO = "xwininfo"
+  
+  #The command to start xkill. 
+  XKILL = "xkill"
+  
+  #The command to start eject. 
+  EJECT = "eject"
+  
+  #Class for errors in this library. 
+  class XError < StandardError
+  end
+  
+end #module XDo
