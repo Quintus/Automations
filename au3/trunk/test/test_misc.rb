7b91e5751d8a4174b1147d7b38ef61dceb92101f
Quintus <sutniuq@gmx.net>
2009-12-15 20:26:02 +0100

    Renamed test_tray.rb to test_misc.rb; added sleep test

--- au3/trunk/test/test_misc.rb
+++ au3/trunk/test/test_misc.rb
@@ -1,6 +1,6 @@
-#Encoding: Windows-1252
+#Encoding: UTF-8
 #This file is part of au3. 
-#Copyright (c) 2009 Marvin Gülker
+#Copyright (c) 2009 Marvin GÃ¼lker
 begin
   require_relative "../lib/au3"
 rescue LoadError
@@ -9,21 +9,28 @@ rescue LoadError
 end
 require "test/unit"
 
-class CDTrayTest < Test::Unit::TestCase
+class MiscTest < Test::Unit::TestCase
   
   TEST_TRAY = "E:"
   
-  def test_open
+  def test_cd_open
     assert(AutoItX3.open_cd_tray(TEST_TRAY))
   end
   
-  def test_close
+  def test_cd_close
     AutoItX3.open_cd_tray(TEST_TRAY)
     if !AutoItX3.close_cd_tray(TEST_TRAY)
       notify "Could not close your CD tray. Are you running on a laptop?"
     else
       assert(true)
     end
+  end
+  
+  def test_sleep
+    than = Time.now
+    AutoItX3.msleep(1000)
+    now = Time.now
+    assert_in_delta(1.0, now - than, 0.01)
   end
   
 end
