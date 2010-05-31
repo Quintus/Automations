42f6aa53c182ad53b55fe95531e8eccde82343f6
Quintus <sutniuq@gmx.net>
2010-05-31 17:21:57 +0200

    Enhanced documentation

--- au3/trunk/lib/AutoItX3/window.rb
+++ au3/trunk/lib/AutoItX3/window.rb
@@ -231,6 +231,8 @@ module AutoItX3
     #Returns an array of all used window classes of +self+. 
     #===Return value
     #An array containg all the window classes as strings. 
+    #===Raises
+    #[Au3Error] Window not found. 
     #===Example
     #  p win.class_list 
     #  #=> ["SciTEWindowContent", "Scintilla", "Scintilla", "ToolbarWindow32", "SciTeTabCtrl", "msctls_statusbar32"]
@@ -243,9 +245,16 @@ module AutoItX3
       buffer.normal.split("\n").map{|str| str.strip.empty? ? nil : str.strip}.compact
     end
     
-    #Returns the client area size of +self+ as a two-element array of 
-    #form <tt>[ width , height ]</tt>. Returns <tt>[0, 0]</tt> on minimized 
+    #Gets the size of a window's client area. 
+    #===Return value
+    #A two-element array of form <tt>[ width , height ]</tt>. Returns <tt>[0, 0]</tt> on minimized 
     #windows. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  p win.client_size #=> [784, 564]
+    #  #Minimized: 
+    #  p win.client_size #=> [0, 0]
     def client_size
       Window.functions[:client_size_width] ||= AU3_Function.new("WinGetClientSizeWidth", 'SS', 'L')
       Window.functions[:client_size_height] ||= AU3_Function.new("WinGetClientSizeHeight", 'SS', 'L')
@@ -254,9 +263,16 @@ module AutoItX3
       size
     end
     
-    #Returns the numeric handle of a window as a string. It can be used 
-    #with the WinTitleMatchMode option set to advanced or for direct calls 
-    #to the windows API (but you have to call <tt>.to_i(16)</tt> on the string then). 
+    #Returns the handle of a window. 
+    #===Return value
+    #Returns the numeric handle of a window as a string. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  p win.handle #=> "00070388"
+    #===Remarks
+    #You may use the handle for instead of passing a title to Window.new or directly for 
+    #Win32API calls (make sure you call <tt>.to_i(16)</tt> on the string before).  
     def handle
       Window.functions[__method__] ||= AU3_Function.new("WinGetHandle", 'SSPI')
       buffer = " " * AutoItX3::BUFFER_SIZE
@@ -266,8 +282,18 @@ module AutoItX3
       buffer.normal.strip
     end
     
+    #Gets a window's rectangle. 
+    #===Return value
     #Returns the position and size of +self+ in a four-element array 
-    #of form <tt>[x, y, width, height]</tt>. 
+    #of form <tt>[x, y, width, height]</tt>. If called on a minimized window, 
+    #the X and Y values are nonsense, but +width+ and +height+ indicate the 
+    #size of the window's bar in the taskbar. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  p win.rect #=> [240, 191, 800, 600]
+    #  #Called on a minimized window: 
+    #  p win.rect #=> [4294935296, 4294935296, 160, 25]
     def rect
       Window.functions[:xpos] ||= AU3_Function.new("WinGetPosX", 'SS', 'L')
       Window.functions[:ypos] ||= AU3_Function.new("WinGetPosY", 'SS', 'L')
@@ -287,8 +313,14 @@ module AutoItX3
       rect
     end
     
+    #Gets the PID of the process running a window. 
+    #===Return value
     #Returns the process identification number of +self+'s window 
     #procedure. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  p win.pid #=> 3128
     def pid
       Window.functions[__method__] ||= AU3_Function.new("WinGetProcess", 'SSPI', 'L')
       buffer = " " * AutoItX3::BUFFER_SIZE
@@ -303,7 +335,10 @@ module AutoItX3
       end
     end
     
-    #Returns the integer composition of the states 
+    #Checks a window's state. You shouldn't use this function, use #exists?, 
+    ##visible?, #enabled?, #active?, #minimized? and #maximized? instead. 
+    #===Return value
+    #Returns the integer composition of the states: 
     #- exists (1)
     #- visible (2)
     #- enabled (4)
@@ -311,8 +346,12 @@ module AutoItX3
     #- minimized (16)
     #- maximized (32)
     #Use the bit-wise AND operator & to check for a specific state. 
-    #Or just use one of the predefined methods #exists?, #visible?, 
-    ##enabled?, #active?, #minimized? and #maximized?. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  #Check for visibility
+    #  p(win.state & 2) #=> 2 #visible
+    #  p(win.state & 2) #=> 0 #invisible
     def state
       Window.functions[__method__] ||= AU3_Function.new("WinGetState", 'SS', 'L')
       state = Window.functions[__method__].call(@title.wide, @text.wide)
@@ -320,28 +359,59 @@ module AutoItX3
       state
     end
     
-    #Returns true if +self+ is shown on the screen. 
+    #Checks wheather a window is visible or not. 
+    #===Return value
+    #Returns true if +self+ is shown on the screen, false otherwise. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  p win.visible? #=> true
     def visible?
       (state & 2) == 2
     end
     
-    #Returns true if +self+ is enabled (i.e. it can receive input). 
+    #Checks wheather a window can receive user input or not. 
+    #===Return value
+    #Returns true if +self+ is enabled, false otherwise. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  p win.enabled? #=> false
     def enabled?
       (state & 4) == 4
     end
     
-    #Returns true if +self+ is minimized to the taskbar. 
+    #Checks wheather or not a window is minimized to the taskbar. 
+    #===Return value
+    #Returns true if +self+ is minimized to the taskbar, false otherwise. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  p win.minimized? #=> true
     def minimized?
       (state & 16) == 16
     end
     
+    #Checks wheather or not this is a fullscreen window. 
+    #===Return value
     #Returns true if +self+ is maximized to full screen size. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  p win.maximized? #=> false
     def maximized?
       (state & 32) == 32
     end
     
     #Returns the text read from a window. This method doesn't query the @text instance 
     #variable, rather it calls the AU3_WinGetText function. 
+    #===Return value
+    #The window's text. It doesn't necassary make sense. 
+    #===Example
+    #  #Used on an explorer window: 
+    #  p win.text #=> "Navigationsleiste\nAdresse: C:\\Users\\marvin_g\\Desktop\\Automations\\au3\\trunk\\lib\nlib\nHostwrapper f├╝r gemeinsame Orte\nShellView\nFolderView\nMen├╝leiste"
+    #  #Some windows just don't have any text. 
+    #  p win.text #=> ""
     def text
       Window.functions[__method__] ||= AU3_Function.new("WinGetText", 'SSPI')
       buffer = " " * AutoItX3::BUFFER_SIZE
@@ -354,6 +424,10 @@ module AutoItX3
     #affect or even use the value of @title, that means you can use 
     #+title+ to retrieve titles from a window if you're working with the 
     #advanced window mode. 
+    #===Return value
+    #The window's title. 
+    #===Example
+    #  p win.title #=> "AutoItX Help"
     def title
       Window.functions[__method__] ||= AU3_Function.new("WinGetTitle", 'SSPI')
       buffer = " " * AutoItX3::BUFFER_SIZE
@@ -364,17 +438,36 @@ module AutoItX3
     
     #Kills +self+. This method forces a window to close if it doesn't close 
     #quickly enough (in contrary to #close which waits for user actions 
-    #if neccessary). Some windows cannot be +kill+ed (notably 
-    #Windows Explorer windows). 
+    #if neccessary). 
+    #===Return value
+    #nil. 
+    #===Example
+    #  win.kill
+    #  p win.exists? #=> false
+    #===Remarks
+    #Some windows cannot be +kill+ed (notably Windows Explorer windows). 
     def kill
       Window.functions[__method__] ||= AU3_Function.new("WinKill", 'SS', 'L')
       Window.functions[__method__].call(@title.wide, @text.wide)
       nil
     end
     
-    #Clicks the specified item in the specified menu. You may specify up to seven 
-    #submenus (1 Menu + 7 submenus). Note: You can't open a menu with this method, 
-    #the last submenu has to be associated with an action like opening a dialog window. 
+    #Clicks a menu entry. 
+    #===Parameters
+    #[+menu+] The name of the top menu, such as <tt>"File"</tt> (or rather <tt>"&File"</tt>; you have to prefix underlined letters with an ampersand sign &). 
+    #[<tt>*items</tt>] Up to 7 submenus, use this in the same matter as +menu+. 
+    #===Return value
+    #nil. 
+    #===Raises
+    #[Au3Error] Window not found. 
+    #===Example
+    #  #Open the "Help" entry in SciTE's "Help" menu: 
+    #  win.select_menu_item("&Help", "&Help")
+    #===Remarks
+    #You can't open a menu with this method, the last submenu has to be associated with an action like opening a dialog window. 
+    #
+    #If you experience troubles with entries containing three dots <tt>...</tt> you should check if those three dots aren't a 
+    #Unicode character like Horizontal Ellipsis (<tt>…</tt>, U+2026). Just try that character instead of three dots. 
     def select_menu_item(menu, *items)
       Window.functions[__method__] ||= AU3_Function.new("WinMenuSelectItem", 'SSSSSSSSSS', 'L')
       raise(ArgumentError, "Wrong number of arguments, maximum is seven items!") if items.size > 7 #(menu is the 8th)
@@ -385,8 +478,24 @@ module AutoItX3
       nil
     end
     
-    #Moves a window (and optionally resizes it). This does not work 
-    #with minimized windows. 
+    #Moves a window (and optionally resizes it). 
+    #===Parameters
+    #[+x+] The X coordinate to move the window to. 
+    #[+y+] The Y coordinate to move the window to. 
+    #[+width+] (<tt>-1</tt>) The window's new width. 
+    #[+height+] (<tt>-1</tt>) The window's new width. 
+    #===Return value
+    #nil. 
+    #===Example
+    #  #Move a window to (10|10): 
+    #  win.move(10, 10)
+    #  #Moves a windot to (100|100) and resizes it to 500 x 500: 
+    #  win.move(100, 100, 500, 500)
+    #  #Since you can't resize a window without moving it, use this to achieve 
+    #  #the same effect: 
+    #  win.move(win.rect[0], win.rect[1], 700, 700)
+    #===Remarks
+    #This does not work with minimized and maximized windows. 
     def move(x, y, width = -1, height = -1)
       Window.functions[__method__] ||= AU3_Function.new("WinMove", 'SSLLLL', 'L')
       Window.functions[__method__].call(@title.wide, @text.wide, x, y, width, height)
@@ -394,7 +503,13 @@ module AutoItX3
     end
     
     #Turn the TOPMOST flag of +self+ on or off. If activated, the window 
-    #will stay on top above all other windows.
+    #will stay on top above all other windows. 
+    #===Parameters
+    #[+val+] true or false. 
+    #===Return value
+    #The argument passed. 
+    #===Example
+    #  win.set_on_top = true
     def set_on_top=(val)
       Window.functions[__method__] ||= AU3_Function.new("WinSetOnTop", 'SSL', 'L')
       Window.functions[__method__].call(@title.wide, @text.wide, !!val)
