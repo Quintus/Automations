c578d30fd096fe123ac3fef91c5ba0d3829d7658
Quintus <sutniuq@gmx.net>
2010-05-31 18:02:34 +0200

    Updated copyright notices

--- au3/trunk/lib/AutoItX3/mouse.rb
+++ au3/trunk/lib/AutoItX3/mouse.rb
@@ -1,6 +1,6 @@
 #Encoding: UTF-8
 #This file is part of au3. 
-#Copyright © 2009 Marvin Gülker
+#Copyright © 2009, 2010 Marvin Gülker, Steven Heidel
 #
 #au3 is published under the same terms as Ruby. 
 #See http://www.ruby-lang.org/en/LICENSE.txt
--- au3/trunk/lib/au3.rb
+++ au3/trunk/lib/au3.rb
@@ -1,2 +1,8 @@
 #Encoding: UTF-8
+#This file is part of au3. 
+#Copyright © 2009, 2010 Marvin Gülker, Steven Heidel
+#
+#au3 is published under the same terms as Ruby. 
+#See http://www.ruby-lang.org/en/LICENSE.txt
+
 require_relative("AutoItX3/au3.rb")
