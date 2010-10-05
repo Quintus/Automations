8dbd6d6b1d6f0fe791241f486fa4392b19d59f93
Quintus <sutniuq@gmx.net>
2010-10-05 15:05:00 +0200

    Removed the silly hash use from the Clipboard module's methods. As I already did with Mouse and XWindow, you can now pass in the symbols directly as a *rest argument.

--- xdo/lib/xdo/clipboard.rb
+++ xdo/lib/xdo/clipboard.rb
@@ -1,4 +1,8 @@
 #Encoding: UTF-8
+#This file is part of Xdo. 
+#Copyright © 2009, 2010 Marvin Gülker
+#  Initia in potestate nostra sunt, de eventu fortuna iudicat. 
+
 require_relative("../xdo")
 
 module XDo
@@ -8,100 +12,195 @@ module XDo
   #the SECONDARY clipboard. The clipboard you access normally via [CTRL]+[C] 
   #or by right-clicking and selecting "copy", is usually the CLIPBOARD clipboard (but that 
   #depends on the application you use). The three main methods of this module (#read, #write 
-  #and #clear) take a hash with the symbols of the clipboards to interact with. If you don't want to 
+  #and #clear) take a list symbols of the clipboards to interact with. If you don't want to 
   #pass in the symbols, use the predefined read_xy, write_xy and clear_xy methods. They cannot 
   #access more than one clipboard at a time. 
   #The symbols for the clipboards are: 
   #[PRIMARY] :primary
   #[SECONDARY] :secondary
   #[CLIPBOARD] :clipboard
+  #You cannot store complex objects like images via this interface, only strings. However, 
+  #you could translate an image into a string (packed pixels maybe?) and put that on the 
+  #clipboard -- for your own application this may be fine, but it won't magically allow 
+  #a user to paste that image into a graphics program. 
+  #
+  #The +xsel+ program used by this module is quite outdated. As far as I can see, it's 
+  #last update happened in 2002 and since I do not believe that software exists that 
+  #won't break over a period of 8 years without a single modification while updating systems I'm about to 
+  #switch to a newer one. +xclip+ is likely, but that one got it's last update in early 
+  #2009... 
   module Clipboard
     
     class << self
       
       ##
       # :singleton-method: read_primary
+      #Returns the contents of the PRIMARY clipboard. 
+      #See #read for an explanation. 
       
       ##
       # :singleton-method: read_clipboard
+      #Returns the contents of the CLIPBOARD clipboard. 
+      #See #read for an explanation. 
       
       ##
       # :singleton-method: read_secondary
+      #Returns the contents of the SECONDARY clipboard. 
+      #See #read for an explanation. 
       
       ##
       # :singleton-method: write_primary
+      #Writes to the PRIMARY clipboard. 
+      #See #write for an explanation. 
       
       ##
       # :singleton-method: write_clipboard
+      #Writes to the CLIPBOARD clipboard. 
+      #See #write for an explanation. 
       
       ##
       # :singleton-method: write_secondary
+      #Writes to the SECONDARY clipboard. 
+      #See #write for an explanation. 
       
       ##
       # :singleton-method: clear_primary
+      #Clears the PRIMARY clipboard. 
+      #See #clear for an explanation. 
       
       ##
       # :singleton-method: clear_clipboard
+      #Clears the CLIPBOARD clipboard. 
+      #See #clear for an explanation. 
       
       ##
-      # :singleton-method: clear_secondary
+      # :singleton-method: clear_secondary     
+      #Clears the SECONDARY clipboard. 
+      #See #clear for an explanation. 
+      
+      #Reads text from a X clipboard. 
+      #===Parameters
+      #[<tt>*from</tt>] (<tt>:clipboard</tt>, <tt>:primary</tt>, <tt>:secondary</tt>) Specifies from which clipboards you want to read (in 70% of all cases you want to read from <tt>:clipboard</tt>). 
+      #===Return value
+      #A hash of form
+      #  {:clip_sym => "clipboard_content"}
+      #If you didn't pass any arguments to #read, the hash will contain keys for 
+      #all clipboard, i.e. for <tt>:clipboard</tt>, <tt>:primary</tt> and <tt>:secondary</tt>. 
+      #If you did, only those symbols will be included you passed. See 
+      #the _Example_ section for an example of this. 
+      #===Example
+      #  XDo::Clipboard.read #| {:clipboard => "...", :primary => "...", :secondary => "..."}
+      #  XDo::Clipboard.read(:primary) #| {:primary => "..."}
+      #  XDo::Clipboard.read(:clipboard, :secondary) #| {clipboard => "...", :secondary => "..."}
+      #===Remarks
+      #You could also use one of the read_* methods for convenience.       
+      def read(*from)
+        if from.first.kind_of? Hash
+          warn("#{caller.first}: Deprecation warning: Use symbols as a rest argument now!")
+          from = from.first.keys
+        end
+        from.concat([:clipboard, :primary, :secondary]) if from.empty?
+        
+        hsh = {}
+        hsh[:primary] = `#{XSEL}` if from.include? :primary
+        hsh[:clipboard] = `#{XSEL} -b` if from.include? :clipboard
+        hsh[:secondary] = `#{XSEL} -s` if from.include? :secondary
+        hsh
+      end
+        
+      
+      #Writes text to a X clipboard. 
+      #===Parameters
+      #[<tt>*to</tt>] (<tt>:clipboard</tt>) Specifies to what clipboards you want to wrote to. 
+      #===Return value
+      #The text written. 
+      #===Example
+      #  XDo::Clipboard.write("I love Ruby") #You can now paste this via [CTRL] + [V]
+      #  XDo::Clipboard.write("I love Ruby", :primary) #You can now paste this via a middle-mouse-button click
+      #  XDo::Clipboard.write("I love Ruby", :clipboard, :primary) #Both of the above
+      #===Remarks
+      #You could also use one of the write_* methods for convenience. 
+      def write(text, *to)
+        if to.first.kind_of? Hash
+          warn("#{caller.first}: Deprecation warning: Use symbols as a rest argument now!")
+          to = to.first.keys
+        end
+        to << :clipboard if to.empty?
+        
+        IO.popen("xsel -i", "w"){|io| io.write(text)} if to.include? :primary
+        IO.popen("xsel -b -i", "w"){|io| io.write(text)} if to.include? :clipboard
+        IO.popen("xsel -s -i", "w"){|io| io.write(text)} if to.include? :secondary
+        text
+      end
+      
+      #Appends text to a X clipboard. 
+      #===Parameters
+      #[+text+] The text to append. 
+      #[<tt>*to</tt>] (<tt>:clipboard</tt>) The clipboards to which you want to append. 
+      #===Return value
+      #Undefined. 
+      #===Example
+      #  XDo::Clipboard.write("I love ")
+      #  XDo::Clipboard.append("Ruby")
+      #  puts XDo::Clipboard.read(:clipboard)[:clipboard] #=> I love Ruby
+      #  
+      #  XDo::Clipboard.write("I love", :primary)
+      #  XDo::Clipboard.append("Ruby", :primary, :clipboard)
+      #  #If you now paste via [CTRL] + [V], you'll get 'Ruby'. If you 
+      #  #paste via the middle mouse button, you'll get 'I love Ruby' 
+      #  #(Assuming you didn't execute the first block of code, of course). 
+      def append(text, *to)
+        if to.first.kind_of? Hash
+          warn("#{caller.first}: Deprecation warning: Use symbols as a rest argument now!")
+          to = to.first.keys
+        end
+        to << :clipboard if to.empty?
+        
+        IO.popen("xsel -a -i", "w"){|io| io.write(text)} if to.include? :primary
+        IO.popen("xsel -b -a -i", "w"){|io| io.write(text)} if to.include? :clipboard
+        IO.popen("xsel -s -a -i", "w"){|io| io.write(text)} if to.include? :secondary
+      end
+      
+      #Clears the specified clipboards. 
+      #===Parameters
+      #[<tt>*clips</tt>] (<tt>:primary</tt>, <tt>:clipboard</tt>, <tt>:secondary</tt>) The clipboards you want to clear. 
+      #===Return value
+      #nil. 
+      #===Example
+      #  XDo::Clipboard.write("I love Ruby")
+      #  XDo::Clipboard.clear
+      #  #Nothing can be pasted anymore
+      #  
+      #  XDo::Clipboard.write("I love Ruby", :clipboard, :primary)
+      #  XDo::Clipboard.clear(:primary)
+      #  #You can still paste via [CTRL] + [V], but not with the middle mouse button
+      def clear(*clips)
+        if clips.first.kind_of? Hash
+          warn("#{caller.first}: Deprecation warning: Use symbols as a rest argument now!")
+          clips = clips.first.keys
+        end
+        clips.concat([:primary, :clipboard, :secondary]) if clips.empty?
+        
+        `#{XSEL} -c` if clips.include? :primary
+        `#{XSEL} -b -c` if clips.include? :clipboard
+        `#{XSEL} -s -c` if clips.inclde? :secondary
+        nil
+      end
       
       [:primary, :clipboard, :secondary].each do |sym|
         
         define_method(:"read_#{sym}") do
-          read({sym => true})[sym]
+          read(sym)[sym]
         end
         
         define_method(:"write_#{sym}") do |text|
-          write(text, {sym => true})
-          text
+          write(text, sym)
         end
         
         define_method(:"clear_#{sym}") do
-          clear({sym => true})
-          nil
+          clear(sym)
         end
         
-      end
-      
-      #Reads text from the X clipboard. The +from+ argument specifies 
-      #from what clipboard you want to read (in 70% of all cases you 
-      #want to read from :clipboard). Return value is a hash with the 
-      #clipboards you specified as the keys. The contents of the clipboards 
-      #are the values. You can also use one of the read_xy methods directly. 
-      def read(from = {:clipboard => true})
-        hsh = {}
-        hsh[:primary] = `#{XSEL}` if from[:primary]
-        hsh[:clipboard] = `#{XSEL} -b` if from[:clipboard]
-        hsh[:secondary] = `#{XSEL} -s` if from[:secondary]
-        hsh
-      end
-      
-      #Writes data to the X clipboard. The +to+ argument soecifies 
-      #the clipboard you want to write to. If you want to be able to paste 
-      #your text via [CTRL]+[v], use :clipboard. You can also use the 
-      #write_xy methods if you don't want to pass in the hash. 
-      def write(text, to = {:primary => true, :clipboard => true})
-        IO.popen("xsel -i", "w"){|io| io.write(text)} if to[:primary]
-        IO.popen("xsel -b -i", "w"){|io| io.write(text)} if to[:clipboard]
-        IO.popen("xsel -s -i", "w"){|io| io.write(text)} if to[:secondary]
-        text
-      end
-      
-      #Append data to text already written to the X clipboard. As in #write, 
-      #you can specify the clipboard you want to append text to. 
-      def append(text, to = {:primary => true, :clipboard => true})
-        IO.popen("xsel -a -i", "w"){|io| io.write(text)} if to[:primary]
-        IO.popen("xsel -b -a -i", "w"){|io| io.write(text)} if to[:clipboard]
-        IO.popen("xsel -s -a -i", "w"){|io| io.write(text)} if to[:secondary]
-      end
-      
-      #Clears the specified clipboards. 
-      def clear(clips = {:primary => true, :clipboard => true})
-        `#{XSEL} -c` if clips[:primary]
-        `#{XSEL} -b -c` if clips[:clipboard]
-        `#{XSEL} -s -c` if clips[:secondary]
-        nil
       end
       
     end
